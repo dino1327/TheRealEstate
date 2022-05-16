19f8042747bde60f79843f8c398cc1447fef02b2
Ionut-Alexandru Chersa <a.chersaion@gmail.com>
2022-05-16 07:46:33 +0000

    Marti a fost realizata initializarea variabilelor si constructorului

--- main/java/org/fis2022/models/User.java
+++ main/java/org/fis2022/models/User.java
@@ -5,9 +5,19 @@ import org.fis2022.exceptions.InvalidCredentials;
 import java.util.Date;
 
 public class User {
+    private String username;
+    private String password;
+    private String role;
+
     public User() {
     }
 
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+    
     @Override
     public boolean equals(Object o) {
 
