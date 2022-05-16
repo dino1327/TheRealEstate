bad1ab58d1e27909190cada7e33580b1c95805fd
Ionut-Alexandru Chersa <a.chersaion@gmail.com>
2022-05-16 06:08:45 +0000

    marti s-au adaugat importurile si package de tip org necesare

--- main/java/org/fis2022/app.java
+++ main/java/org/fis2022/app.java
@@ -1,4 +1,8 @@
+package org.fis2022;
 
+import org.fis2022.services.DBService;
+import org.fis2022.services.FileSystemService;
+import org.fis2022.services.UserService;
 import java.nio.file.Path;
 
 public class App extends Application {
