57ebfee8d1edf44b1e8c078f6b32c585c32b994a
Ionut-Alexandru Chersa <a.chersaion@gmail.com>
2022-05-16 06:03:29 +0000

    Sesiune de lucru aferanta saptamanii 8, luni s-au creeat folderele necesare si s-a creeat sablon pentru aplicatia principala

---
+++ main/java/org/fis2022/app.java
@@ -0,0 +1,11 @@
+
+import java.nio.file.Path;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+
+    public static void main(String[] args) {
+    }
+}
