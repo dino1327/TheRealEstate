fcdd3867997ad450df8fee514d3ea125e70b072f
Ionut-Alexandru Chersa <a.chersaion@gmail.com>
2022-05-16 06:43:13 +0000

    Miercuri toate importurile au fost rezolvateimporturile pentru setarea scenei au fost rezolvate impreuna cu cele fxml

--- main/java/org/fis2022/controllers/Autentificare.java
+++ main/java/org/fis2022/controllers/Autentificare.java
@@ -1,5 +1,15 @@
 package org.fis2022.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 import org.fis2022.exceptions.InvalidCredentials;
 import org.fis2022.models.User;
 import org.fis2022.services.UserService;
